c920ba991c0df2c7aa222913d9bd81f244196612
MathisWellmann <wellmannmathis@gmail.com>
2025-03-03 13:59:32 +0100

    benches: add `roc.rs` benchmark

---
+++ benches/roc.rs
@@ -0,0 +1,33 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::{rng, Rng};
+use sliding_features::{pure_functions::Echo, sliding_windows::Roc, View};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = rng();
+    const N: usize = 100_000;
+
+    let mut group = c.benchmark_group("roc_100k");
+    group.bench_function("f64", |b| {
+        let vals = Vec::<f64>::from_iter((0..N).map(|_| rng.random()));
+        b.iter(|| {
+            let mut view = Roc::<f64, _>::new(Echo::new(), 1024);
+            for v in vals.iter() {
+                view.update(*v);
+                let _ = black_box(view.last());
+            }
+        })
+    });
+    group.bench_function("f32", |b| {
+        let vals = Vec::<f32>::from_iter((0..N).map(|_| rng.random()));
+        b.iter(|| {
+            let mut view = Roc::<f32, _>::new(Echo::new(), 1024);
+            for v in vals.iter() {
+                view.update(*v);
+                let _ = black_box(view.last());
+            }
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
