1d156b9780f32f104a81bc46a76aa0117e804435
MathisWellmann <wellmannmathis@gmail.com>
2025-03-03 12:50:19 +0100

    benches: add `correlation_trend_indicator.rs` benchmark

---
+++ benches/correlation_trend_indicator.rs
@@ -0,0 +1,33 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand::{rng, Rng};
+use sliding_features::{pure_functions::Echo, sliding_windows::CorrelationTrendIndicator, View};
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut rng = rng();
+    const N: usize = 1_000_000;
+
+    let mut group = c.benchmark_group("correlation_trend_indicator_1M");
+    group.bench_function("f64", |b| {
+        let vals = Vec::<f64>::from_iter((0..N).map(|_| rng.random()));
+        b.iter(|| {
+            let mut alma = CorrelationTrendIndicator::<f64, _>::new(Echo::new(), 1024);
+            for v in vals.iter() {
+                alma.update(*v);
+                let _ = black_box(alma.last());
+            }
+        })
+    });
+    group.bench_function("f32", |b| {
+        let vals = Vec::<f32>::from_iter((0..N).map(|_| rng.random()));
+        b.iter(|| {
+            let mut alma = CorrelationTrendIndicator::<f32, _>::new(Echo::new(), 1024);
+            for v in vals.iter() {
+                alma.update(*v);
+                let _ = black_box(alma.last());
+            }
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
