7f73d509f45c98057300f417a6acacc6961e12cf
MathisWellmann <wellmannmathis@gmail.com>
2025-03-03 12:44:53 +0100

    benches: add `center_of_gravity.rs` benchmark

--- benches/center_of_gravity.rs
+++ benches/center_of_gravity.rs
@@ -1,16 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::{rng, Rng};
-use sliding_features::{pure_functions::Echo, sliding_windows::Alma, View};
+use sliding_features::{pure_functions::Echo, sliding_windows::CenterOfGravity, View};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let mut rng = rng();
     const N: usize = 1_000_000;
 
-    let mut group = c.benchmark_group("alma_1M");
+    let mut group = c.benchmark_group("center_of_gravity_1M");
     group.bench_function("f64", |b| {
         let vals = Vec::<f64>::from_iter((0..N).map(|_| rng.random()));
         b.iter(|| {
-            let mut alma = Alma::<f64, _>::new(Echo::new(), 1024);
+            let mut alma = CenterOfGravity::<f64, _>::new(Echo::new(), 1024);
             for v in vals.iter() {
                 alma.update(*v);
                 let _ = black_box(alma.last());
@@ -20,7 +20,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.bench_function("f32", |b| {
         let vals = Vec::<f32>::from_iter((0..N).map(|_| rng.random()));
         b.iter(|| {
-            let mut alma = Alma::<f32, _>::new(Echo::new(), 1024);
+            let mut alma = CenterOfGravity::<f32, _>::new(Echo::new(), 1024);
             for v in vals.iter() {
                 alma.update(*v);
                 let _ = black_box(alma.last());
